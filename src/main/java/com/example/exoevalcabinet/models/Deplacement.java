e528b4ed93ddbc3f24e9cde63e2a29da32f2c8ed
thomas Heddadji <thomasheddadji0@gmail.com>
2021-09-30 17:32:59 +0200

    create deplacement model

--- src/main/java/com/example/exoevalcabinet/models/Deplacement.java
+++ src/main/java/com/example/exoevalcabinet/models/Deplacement.java
@@ -1,12 +1,25 @@
 package com.example.exoevalcabinet.models;
 
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
 
 
-
+@Entity
+@Data
+@Table(name = "deplacement")
 public class Deplacement {
 
-
+    // id, date, cout
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "cout")
+    private double cout;
 }
