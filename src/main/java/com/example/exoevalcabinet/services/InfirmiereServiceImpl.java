3ecb4028f7e4121e0677cd892d33969a7650be92
thomas Heddadji <thomasheddadji0@gmail.com>
2021-10-01 00:08:04 +0200

    add Infirmiere service and serviceImpl

---
+++ src/main/java/com/example/exoevalcabinet/services/InfirmiereServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.exoevalcabinet.services;
+
+import com.example.exoevalcabinet.models.Infirmiere;
+
+import java.util.List;
+import java.util.Optional;
+
+public class InfirmiereServiceImpl implements InfirmiereService {
+
+    private
+
+    @Override
+    public List<Infirmiere> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Infirmiere> findOneById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Infirmiere create(Infirmiere infirmiere) {
+        return null;
+    }
+
+    @Override
+    public Infirmiere put(Infirmiere infirmiere) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
