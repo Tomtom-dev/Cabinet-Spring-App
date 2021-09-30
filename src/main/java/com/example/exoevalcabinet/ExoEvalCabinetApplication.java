d6710971228a9e8a72683db8951c4206b48e948b
thomas Heddadji <thomasheddadji0@gmail.com>
2021-09-30 16:19:49 +0200

    initialize the project

---
+++ src/main/java/com/example/exoevalcabinet/ExoEvalCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.exoevalcabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoEvalCabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExoEvalCabinetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exoevalcabinet/ExoEvalCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exoevalcabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoEvalCabinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
